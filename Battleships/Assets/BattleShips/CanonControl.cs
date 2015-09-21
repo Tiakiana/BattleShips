4d04c7b4c28c3b1d5ac01f40ab1297b3c4fcf026
jakob arndal <takana@exelion.dk>
2015-09-21 22:42:59 +0200

    Første Kanon smidt på skibet
    7 more to go

---
+++ Battleships/Assets/BattleShips/CanonControl.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CanonControl : MonoBehaviour
+{
+    public GameObject[] Canons = new GameObject[8];
+
+    void FireCanon(int cn)
+    {
+        switch (cn)
+        {
+            case 1:
+
+                break;
+
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            case 4:
+
+                break;
+            case 5:
+
+                break;
+            case 6:
+
+                break;
+            case 7:
+
+                break;
+            case 8:
+
+                break;
+            default:
+                break;
+        }
+    }
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown("7"))
+        {
+
+
+        }
+    }
+}
