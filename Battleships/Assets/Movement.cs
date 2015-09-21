7036516893ec16af936cbcd46204aa542392181b
jakob arndal <takana@exelion.dk>
2015-09-21 13:57:13 +0200

    Movement Done
    Så er movement done

--- Battleships/Assets/Movement.cs
+++ Battleships/Assets/Movement.cs
@@ -1,23 +1,69 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Movement : MonoBehaviour {
+public class Movement : MonoBehaviour
+{
     Rigidbody rb = null;
-
-	// Use this for initialization
-	void Start () {
+    //Input input;
+    float minimumSpeed, maximumSpeed, currentSpeed, desiredSpeed, turnSpeed;
+    // Use this for initialization
+    void Start()
+    {
         rb = gameObject.AddComponent<Rigidbody>();
         rb.useGravity = false;
-	}
+        rb.constraints = RigidbodyConstraints.FreezeRotationZ;
+        rb.constraints = RigidbodyConstraints.FreezeRotationX;
+        minimumSpeed = 1;
+        maximumSpeed = 5;
+        currentSpeed = 1;
+        desiredSpeed = 1;
+        turnSpeed = 0.5F;
 
-   
+        //input = new Input();
+    }
 
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetKeyDown("w")) {
-            rb.AddForce(Vector3.forward*10,ForceMode.Acceleration);
 
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * currentSpeed * Time.deltaTime);
+        if (currentSpeed > desiredSpeed)
+        {
+            currentSpeed -= 0.05F;
+        }
+        if (currentSpeed < desiredSpeed)
+        {
+            currentSpeed += 0.05F;
         }
 
-    }
+        if (Input.GetKeyDown("w"))
+        {
+            if (desiredSpeed < maximumSpeed)
+            {
+                desiredSpeed += 1;
+            }
+        }
+        else if (Input.GetKeyDown("s"))
+        {
+            if (desiredSpeed > minimumSpeed)
+            {
+                desiredSpeed -= 1;
+            }
+        }
+        else
+        {
+            if (Input.GetKey("a"))
+            {
+                transform.Rotate(Vector3.down * turnSpeed, Space.World);
+            }
+        
+         else
+        {
+            if (Input.GetKey("d"))
+            {
+                transform.Rotate(Vector3.up * turnSpeed, Space.World);
+            }
+        }
+    } }
 }
