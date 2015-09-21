dae1cbdcbf02124a08213997eba9e78808ac0684
jakob arndal <takana@exelion.dk>
2015-09-21 11:45:49 +0200

    Voldspalle 1
    Og hans makker

---
+++ Battleships/Assets/Movement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+    Rigidbody rb = null;
+
+	// Use this for initialization
+	void Start () {
+        rb = gameObject.AddComponent<Rigidbody>();
+        rb.useGravity = false;
+	}
+
+   
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("w")) {
+            rb.AddForce(Vector3.forward*10,ForceMode.Acceleration);
+
+        }
+
+    }
+}
