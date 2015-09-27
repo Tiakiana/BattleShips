652a30e352c6d571f8a18dc887ee26dedc5d8283
Tiakiana <takana@exelion.dk>
2015-09-27 11:04:26 +0200

    Astar Implementering
    A* Som vi kender det. Mangler selv aien til at udregne, men grid og
    tiles er færdige

---
+++ project nexus/Assets/AStar/AstarAlgorithm.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AstarAlgorithm : MonoBehaviour {
+    List<GameObject> open;
+    List<GameObject> closed;
+
+    void Start () {
+        open = new List<GameObject>();
+        closed = new List<GameObject>();
+	}
+
+    internal void SetStartingPoint(GameObject sp) {
+        open.Add(sp);
+    }
+	
+	void Update () {
+	
+	}
+}
---
+++ project nexus/Assets/AStar/AstarTile.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class AstarTile : MonoBehaviour {
+    public int h;
+    public int f;
+    public int g;
+    public bool walkable = true;
+    public bool closed = false;
+    public List<GameObject> neighbors = new List<GameObject>();
+    public int gridX;
+    public int gridY;
+    void Start() {
+
+    }
+
+    public void AstarTileSet(int _X, int _Y) {
+        gridX = _X;
+        gridY = _Y;
+    }
+    internal void SetColor() {
+        gameObject.GetComponent<Renderer>().material.color = new Color(255, 0, 0);
+    }
+	
+	void Update () {
+	
+	}
+}
